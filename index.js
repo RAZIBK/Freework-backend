e34a20d2227c897d202733a6a154f51a96370e73
Razi bk <razibeppukaran@gmail.com.com>
2022-12-16 01:30:07 +0530

    remove nodeMon

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const  express  = require("express");
-const dotenv=require('dotenv')
-const cors = require ('cors')
-dotenv.config();    
-const dbConnect=require('./config/db/dbconnection');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+const dbConnect = require("./config/db/dbConnection");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const userRoutes = require("./route/users/UserRoutes");
 const clientRoutes = require("./route/users/clientRoutes");
 const projectRoutes = require("./route/project/ProjectRoutes");
 const applicationRoutes = require("./route/applications/applicationRouter");
 
-
-
-
-
-
-const app=express();
-dbConnect()
-
-
+const app = express();
+dbConnect();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/user',userRoutes);
-app.use('/api/client',clientRoutes);
-app.use('/api/project',projectRoutes);
-app.use('/api/application',applicationRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/client", clientRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/application", applicationRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-
-const PORT=process.env.PORT||5000;
-app.listen(PORT,console.log(`server is running at ${PORT}`)); 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server is running at ${PORT}`));
